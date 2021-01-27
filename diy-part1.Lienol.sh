d355c4c4cb4c40f4000ba5d7288da4543d37269a
rabbitwit <71050783+rabbitwit@users.noreply.github.com>
2021-01-27 23:43:58 +0800

    Create diy-part1.Lienol.sh

---
+++ diy-part1.Lienol.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# 修改openwrt-luci为18.06分支
+#sed -i 's/17.01/18.06/g' feeds.conf.default
+
+# 拉取最新版argon主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+# Add a feed source
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
+# 获取luci-app-smartdns
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/lean/luci-app-smartdns
